f108cf1b5d19abda6319fbbc0ca44b65e5ec1e29
To Dat <todat999@gmail.com>
2021-09-17 00:34:32 +0700

    update api register

--- app.js
+++ app.js
@@ -8,17 +8,38 @@ app.use(bodyParser.json()) ;
 app.set('view engine','ejs');
 app.set('views','./pages');
 
-app.get('/home', (req, res) => {
-
+app.get('/', (req, res) => {
+    res.redirect('/dang-ky')
 });
 
+let arrUser = [
+    {
+        username: "phat", 
+        password: "123"
+    }
+];
+
 app.get('/dang-ky', (req, res) => {
-    res.render('register');
+    res.render('register', {arrUser});
 })
-app.post('/dang-ky-1', (req, res ) =>{
+
+app.post('/dang-ky', (req, res ) =>{
     const { username, password } = req.body;
-    res.json({username, password});
-    
+
+    let isTrung = false;
+
+    arrUser.forEach(item => {
+        if(item.username == username){
+            isTrung = true;
+        } 
+    })
+
+    if(isTrung == true){
+        return res.json({error: true, message: "USERNAME DA TON TAIIIIIII"})
+    }else{
+        arrUser.push({ username, password })
+        return res.json(arrUser);
+    }
 })
 
 app.listen(9999, () =>
